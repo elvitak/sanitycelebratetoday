4c93b11558b9acdfcb93f69fdf3c78f58efb7ee3
elvitak <elvita.kalvane@gmail.com>
2024-09-18 13:08:42 +0200

    Add schema for celebrations

---
+++ schemaTypes/celebration.js
@@ -0,0 +1,22 @@
+export default {
+  name: 'celebration',
+  type: 'document',
+  title: 'Celebration',
+  fields: [
+    {
+      name: 'image',
+      type: 'image',
+      title: 'Image',
+    },
+    {
+      name: 'title',
+      type: 'string',
+      title: 'Title',
+    },
+    {
+      name: 'slug',
+      type: 'slug',
+      title: 'Slug',
+    },
+  ],
+}
