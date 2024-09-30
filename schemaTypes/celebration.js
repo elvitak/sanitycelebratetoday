83afbdc9243d5855d3dec336eee63c626d372325
elvitak <elvita.kalvane@gmail.com>
2024-09-30 10:18:47 +0300

    Add requirement to make mandatory fields go back to conent not content because of Sanity issues

--- schemaTypes/celebration.js
+++ schemaTypes/celebration.js
@@ -7,19 +7,22 @@ export default {
       name: 'image',
       type: 'image',
       title: 'Image',
+      validation: (Rule) => Rule.required(),
     },
     {
       name: 'title',
       type: 'string',
       title: 'Title',
+      validation: (Rule) => Rule.required(),
     },
     {
       name: 'slug',
       type: 'slug',
       title: 'Slug',
+      validation: (Rule) => Rule.required(),
     },
     {
-      name: 'content',
+      name: 'conent',
       titile: 'Content',
       type: 'array',
       of: [
